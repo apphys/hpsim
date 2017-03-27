2bb6ee3e92917ca603ff7aac605be1a6353a9f10
Xiaoying Pang <xpang@lanl.gov>
2017-03-27 16:06:16 -0600

    add db & pytest

---
+++ pytest/sim-lbeg.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+#
+# sim-lbeg.py
+# for simulating LBEG beam from point A to B in the LANSCE linac
+#
+import sys
+import os
+# define directory to packages and append to $PATH
+par_dir = os.path.abspath(os.path.pardir)
+print par_dir
+lib_dir = os.path.join(par_dir,"bin")
+print lib_dir
+sys.path.append(lib_dir)
+pkg_dir = os.path.join(par_dir,"pylib")
+print pkg_dir
+sys.path.append(pkg_dir)
+
+#import additional python packages
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import math
+# import additional simulation packages
+import hpsim as hps
+import HPSim as HPSim
+# use next line to select either GPU 0 or 2 on aothpsim
+GPU = 0
+hps.set_gpu(GPU)
+import lcsutil as lcs
+import nputil as npu
+import sqldb as pydb
+
+################################################################################
+# install db's and connect to beamline
+db_dir = par_dir + '/db'
+lib_dir = par_dir + '/db/lib'
+dbs = ['tbtd.db','dtl.db','trst.db','ccl.db']
+dbconn1 = hps.DBConnection(db_dir, dbs, lib_dir, 'libsqliteext.so')
+dbconn1.print_dbs()
+dbconn1.clear_model_index()
+print "*** dB connection established ***"
+
+################################################################################
+# create beamline
+bl = hps.BeamLine()
+beamline = hps.get_element_list()
+print "*** Beamline created ***"
+
+################################################################################
+# create table of beamline elements at lengths
+pybl = pydb.Db_bl(db_dir, dbs)
+py_beamline = pybl.get_bl_elem_len()
+print "*** PySQLite Beamline created ***"
+
+################################################################################
+# create H- beam
+SIM_START = "TBDB02" #defined by input beam location
+#beam = hps.Beam(mass=939.294, charge=-1.0, current=0.015, num=1024*256) #H- beam
+beam = hps.Beam(mass=939.294, charge=-1.0, current=0.015, num=1024*256/4) #H- beam
+beam.set_dc(0.095, 47.0, 0.00327,  -0.102, 60.0, 0.002514, 180.0, 0.0, 0.7518) #TBDB02 20140901
+beam.set_frequency(201.25)
+betalambda = hps.betalambda(mass = beam.get_mass(), freq=beam.get_frequency(), w=0.750)
+phi_offset = -hps.get_beamline_length(SIM_START,'BLZ')/betalambda *360
+beam.set_ref_w(0.750)
+beam.set_ref_phi(phi_offset)
+beam.translate('phi', phi_offset)
+beam.save_initial_beam()
+print "*** H- Beam created ***"
+
+################################################################################
+# create spacecharge
+spch = hps.SpaceCharge(nr = 32, nz = 128, interval = 0.025, adj_bunch = 3)
+print "spch interval=", spch.get_interval()
+print "adj_bunch=", spch.get_adj_bunch()
+# define at what energy simulation stops using adjacent bunches in SC calc
+spch.set_adj_bunch_cutoff_w(0.8)
+# remeshing factor determines how ofter the mesh gets recalc vs scaled for SC kick
+spch.set_remesh_threshold(0.02)
+#spch.set_remesh_threshold(0.2)
+print "cutoff w=", spch.get_adj_bunch_cutoff_w()
+print "*** Space Charge Initialized ***"
+
+################################################################################
+# create simulator
+sim = hps.Simulator(beam)
+sim.set_space_charge('on')
+print "*** Simulator Initialized ***"
+
+################################################################################
+# STANDARD AND REQUIRED STUFF ABOVE THIS LINE
+################################################################################
+
+SIM_STOP = '48DT'
+ENERGY_CUTOFF = 0.0
+mask = gmask = beam.get_good_mask()
+
+print "*** Input Beam ***"
+print SIM_START
+print "w/user units"
+beam.print_results()
+
+print "*** Starting Simulation ***\n"
+sim.simulate(SIM_START, SIM_STOP)
+
+# determine mask of particles used in analysis and plotting
+wmask = beam.get_mask_with_limits('w', lolim = ENERGY_CUTOFF)
+gmask = beam.get_good_mask(wmask)
+mask = gmask
+
+print "*** Output Beam ***"
+print SIM_STOP
+print "w/user units"
+beam.print_results(mask)
+
+# create output plot
+plot = hps.BeamPlot(nrow=4, ncol=3, hsize=16, vsize=12)
+plot.title(SIM_STOP)
+plot.iso_phase_space('xxp', beam, mask, 1)
+plot.iso_phase_space('yyp', beam, mask, 2)
+plot.iso_phase_space('phiw', beam, mask, 3 )
+plot.hist2d_phase_space('xxp', beam, mask, 4)
+plot.hist2d_phase_space('yyp', beam, mask, 5)
+plot.hist2d_phase_space('phiw', beam, mask, 6)
+plot.profile('x', beam, mask, 7, 'g-')
+plot.profile('y', beam, mask, 8, 'g-')
+plot.profile('phi', beam, mask, 9, 'g-')
+plot.profile('xp', beam, mask, 10, 'g-')
+plot.profile('yp', beam, mask, 11, 'g-')
+plot.profile('w', beam, mask, 12, 'g-')
+plot.show()
+exit()
--- src/beam/inc/beam.h
+++ src/beam/inc/beam.h
@@ -1,6 +1,5 @@
 #ifndef BEAM_H
 #define BEAM_H
-
 #include <string>
 #include <vector>
 #include "py_wrapper.h"
@@ -30,7 +29,10 @@ typedef unsigned int uint;
  *
  * All the parallel reduction kernels of the beam class uses the algorithm
  * from <br>
- * Mark Harris, "Optimizing Parallel Reduction in CUDA", NIVDIA, 2007.
+ * Mark Harris, "Optimizing Parallel Reduction in CUDA", NIVDIA, 2007 <br>
+ *
+ * \todo When a particle becomes lost longitudinally mark longitudinal_loss
+ * with the index of the beam line element where the particle is lost.
  */
 class Beam : public PyWrapper
 {
@@ -39,8 +41,7 @@ public:
   Beam(std::string r_file);
   Beam(uint r_num, double r_mass, double r_charge, double r_current);
   ~Beam();
-  void AllocateBeam(uint r_num, double r_mass, double r_charge, 
-		    double r_current);
+  void AllocateBeam(uint r_num, double r_mass, double r_charge, double r_current);
   void InitBeamFromFile(std::string r_file);
   void InitBeamFromDistribution(std::vector<double>& r_x, 
       std::vector<double>& r_xp, std::vector<double>& r_y, 
@@ -54,6 +55,7 @@ public:
   void InitDCBeam(double r_ax, double r_bx, double r_ex, 
       double r_ay, double r_by, double r_ey, double r_dphi,
       double r_sync_phi, double r_sync_w, uint r_seed = 0);
+  void SetNumThreadsPerBlock(uint r_blck_size);
   void SaveInitialBeam();
   void SaveIntermediateBeam();
   void RestoreInitialBeam();
@@ -84,29 +86,34 @@ public:
   void PrintToFile(std::string r_file, std::string r_msg = ""); 
   void Print(uint r_indx); 
   void PrintSimple(); 
-  void SetNumThreadsPerBlock(uint r_blck_size);
   void SetRefEnergy(double);
   void SetRefPhase(double);
   double GetRefEnergy() const;
   double GetRefPhase() const;
+
   double GetAvgX(bool r_good_only = false) const;
   double GetAvgY(bool r_good_only = false) const;
   double GetAvgPhi(bool r_good_only = false) const;
   double GetAvgRelativePhi() const;
   double GetAvgW(bool r_good_only = false) const;
+
   double GetSigX(bool r_good_only = false) const;
   double GetSigY(bool r_good_only = false) const;
   double GetSigPhi() const;
   double GetSigRelativePhi(bool r_good_only = false) const;
   double GetSigW(bool r_good_only = false) const;
   //double GetSigR() const;
+
   double GetEmittanceX() const;
   double GetEmittanceY() const;
   double GetEmittanceZ() const;
+
   double GetMaxPhi() const;
+
   uint GetLossNum() const;
   uint GetLongitudinalLossNum() const;
   uint GetGoodParticleNum() const;
+
   std::vector<double> GetX() const;
   void GetX(double* r_out);
   std::vector<double> GetXp() const;
@@ -137,178 +144,81 @@ public:
   void ChangeFrequency(double r_freq);
 
   // host data
-  //! Number of macro-particles, on host 
-  uint num_particle; 
-  //! Particle rest mass, on host
-  double mass;  
-  //! Particle charge, on host
-  double charge; 
-  //! Beam current, on host in [A]
-  double current; 
-  //! Frequency, used to define phase, on host
-  double freq; 
-  //! Design energy, update during simulation, on host
-  double design_w; 
-  //! Number of threads per block used in kernel launching
-  uint blck_size; 
-  //! Number of blocks used in kernel launching
-  uint grid_size; 
-  //! x coordinates, device pointer to an array of size num_particle
-  double* x; 
-  //! y coordinates, devcie pointer to an array of size num_particle
-  double* y; 
-  //! xp coordinates, device pointer to an array of size num_particle
-  double* xp; 
-  //! yp coordinates, device pointer to an array of size num_particle
-  double* yp; 
-  //! Absolute phases, device pointer to an array of size num_particle
-  double* phi; 
-  //! Phase relative to reference particle, device pointer to an array 
-  //! of size num_particle 
-  double* phi_r; 
-  //! Kinetic energies, device pointer to an array of size num_particle
-  double* w; 
-  //! Transverse loss, device poiner to an array of size num_particle
-  uint* loss; 
-  //! Longitudinal loss, device pointer to an array of size num_particle
-  uint* lloss; 
-  //! x mean, excluding particles transversely lost, device pointer to a number
-  double* x_avg; 
-  //! x mean, excluding particles either transversely or longitudinally lost, 
-  //! device pointer to a number.
-  double* x_avg_good; 
-  //! xp mean, excluding particles transversely lost, device pointer to a number
-  double* xp_avg; 
-  //! y mean, excluding particles transversely lost, device pointer to a number
-  double* y_avg; 
-  //! y mean, excluding particles either transversely or longitudinally lost, 
-  //! device pointer to a number
-  double* y_avg_good;  
-  //! yp mean, excluding particles transversely lost, device pointer to a number
-  double* yp_avg; 
-  //! Absolute phase mean, excluding particles transversely lost, 
-  //! device pointer to a number
-  double* phi_avg; 
-  //! Relative phase mean, excluding particles transversely lost, 
-  //! device pointer to a number
-  double* phi_avg_r; 
-  //! Absolute phase mean, excluding particles either transversely or 
-  //! longitudinally lost, device pointer to a number
-  double* phi_avg_good; 
-  //! Kinetic energy mean, excluding particles transversely lost, 
-  //! device pointer to a number
-  double* w_avg; 
-  //! Kinetic energy mean, excluding particles either transversely or 
-  //! longitudinally lost, device pointer to a number
-  double* w_avg_good; 
-  //! x std, excluding particles transversely lost, device pointer to a number
-  double* x_sig; 
-  //! x std, excluding particles either transversely or longitudinally lost, 
-  //! device pointer to a number
-  double* x_sig_good; 
-  //! xp std, excluding particles transversely lost, device pointer to a number
-  double* xp_sig; 
-  //! y std, excluding particles transversely lost, device pointer to a number
-  double* y_sig; 
-  //! y std, excluding particles either transversely or longitudinally lost, 
-  //! device pointer to a number
-  double* y_sig_good; 
-  //! yp std, excluding particles transversely lost, device pointer to a number
-  double* yp_sig; 
-  //! Absolute phase std, excluding particles transversely lost, 
-  //! device pointer to a number
-  double* phi_sig; 
-  //! Absolute phase std, excluding particles either transversely or 
-  //! longitudinally lost, device pointer to a number
-  double* phi_sig_good; 
-  //! Relative phase std, excluding particles transversely lost, 
-  //! device pointer to a number
-  double* phi_sig_r; 
-  //! Kinetic energy std, excluding particles transversely lost, 
-  //! device pointer to a number
-  double* w_sig; 
-  //! Kinetic energy std, excluding particles either transversely or 
-  //! longitudinally lost, device pointer to a number
-  double* w_sig_good; 
-  //! x emittance, excluding particles transversely lost, 
-  //! device pointer to a number
-  double* x_emit; 
-  //! x emittance, excluding particles either transversely or longitudinally 
-  //! lost, device pointer to a number
-  double* x_emit_good; 
-  //! y emittance, excluding particles transversely lost, device pointer 
-  //! to a number
-  double* y_emit; 
-  //! y emittance, excluding particles either transversely or longitudinally 
-  //! lost, device pointer to a number
-  double* y_emit_good; 
-  //! z emittance, excluding particles transversely lost, device 
-  //! pointer to a number
-  double* z_emit; 
-  //! z emittance, excluding particles either transversely or longitudinally 
-  //! lost, device pointer to a number
-  double* z_emit_good; 
-  //! Maximum transverse beam size, r = sqrt(x^2+y^2). excluding particles 
-  //! transversely lost, device pointer to a number
-  double* r_max; 
-  //! Maximum absolute phase, r = sqrt(x^2+y^2). excluding particles 
-  //! transversely lost, device pointer to a number
-  double* abs_phi_max; 
-  //! Number of transversely lost particles, device pointer to a number
-  uint* num_loss; 
-  //! Number of longitudinally lost particles, device pointer to a number
-  uint* num_lloss; 
-  //! Number of good particles, which means they are not lost either 
-  //! transversely nor longitudinally
-  uint* num_good; 
-  //! Temporary array used in reduce kernels. size = grid_size + 1
-  uint* partial_int1; 
-  //! Temporary array used in reduce kernels. size = (grid_size + 1) * 2
-  double* partial_double2; 
-  //! Temporary array used in reduce kernels. size = (grid_size + 1) * 3
-  double* partial_double3; 
+  uint num_particle; //!< Number of macro-particles, on host
+  double mass;  //!< Particle rest mass, on host
+  double charge; //!< Particle charge, on host
+  double current; //!< Beam current, on host in [A]
+  double freq; //!< Frequency, used to define phase, on host
+  double design_w; //!< Design energy, update during simulation, on host
+  uint blck_size; //!< Number of threads per block used in kernel launching
+  uint grid_size; //!< Number of blocks used in kernel launching
+  // device pointers to arrays, size = num_particle
+  double* x; //!< x coordinates, device pointer to an array of size num_particle
+  double* y; //!< y coordinates, devcie pointer to an array of size num_particle
+  double* xp; //!< xp coordinates, device pointer to an array of size num_particle
+  double* yp; //!< yp coordinates, device pointer to an array of size num_particle
+  double* phi; //!< absolute phases, device pointer to an array of size num_particle
+  double* phi_r; //!< phase relative to reference particle, device_pointer to an array of size num_particle
+  double* w; //!< kinetic energies, device pointer to an array of size num_particle
+  uint* loss; //!< transverse loss, device poiner to an array of size num_particle
+  uint* lloss; //!< longitudinal loss, device pointer to an array of size num_particle
+  // device pointer to a data
+  double* x_avg; //!< x mean, excluding particles transversely lost, device pointer to a number.
+  double* x_avg_good; //!< x mean, excluding particles either transversely or longitudinally lost, device pointer to a number.
+  double* xp_avg; //!< xp mean, excluding particles transversely lost, device pointer to a number.
+  double* y_avg; //!< y mean, excluding particles transversely lost, device pointer to a number.
+  double* y_avg_good;  //!< y mean, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* yp_avg; //!< yp mean, excluding particles transversely lost, device pointer to a number.
+  double* phi_avg; //!< absolute phase mean, excluding particles transversely lost, device pointer to a number.
+  double* phi_avg_r; //!< relative phase mean, excluding particles transversely lost, device pointer to a number.
+  double* phi_avg_good; //!< absolute phase mean, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* w_avg; //!< kinetic energy mean, excluding particles transversely lost, device pointer to a number.
+  double* w_avg_good; //!< kinetic energy mean, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* x_sig; //!< x std, excluding particles transversely lost, device pointer to a number.
+  double* x_sig_good; //!< x std, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* xp_sig; //!< xp std, excluding particles transversely lost, device pointer to a number.
+  double* y_sig; //!< y std, excluding particles transversely lost, device pointer to a number.
+  double* y_sig_good; //!< y std, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* yp_sig; //!< yp std, excluding particles transversely lost, device pointer to a number.
+  double* phi_sig; //!< absolute phase std, excluding particles transversely lost, device pointer to a number.
+  double* phi_sig_good; //!< absolute phase std, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* phi_sig_r; //!< relative phase std, excluding particles transversely lost, device pointer to a number.
+  double* w_sig; //!< kinetic energy std, excluding particles transversely lost, device pointer to a number.
+  double* w_sig_good; //!< kinetic energy std, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* x_emit; //!< x emittance, excluding particles transversely lost, device pointer to a number
+  double* x_emit_good; //!< x emittance, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* y_emit; //!< y emittance, excluding particles transversely lost, device pointer to a number
+  double* y_emit_good; //!< y emittance, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* z_emit; //!< z emittance, excluding particles transversely lost, device pointer to a number
+  double* z_emit_good; //!< z emittance, excluding particles either transversely or longitudinally lost, device pointer to a number
+  double* r_max; //!< maximum transverse beam size, r = sqrt(x^2+y^2). excluding particles transversely lost, device pointer to a number
+  //double* r_sig;
+  double* abs_phi_max; //!< maximum absolute phase, r = sqrt(x^2+y^2). excluding particles transversely lost, device pointer to a number
+  uint* num_loss; //!< number of transversely lost particles, device pointer to a number
+  uint* num_lloss; //!< number of longitudinally lost particles, device pointer to a number
+  uint* num_good; //!< number of good particles, which means they are not lost either transversely nor longitudinally.
+  uint* partial_int1; //!< temporary array used in reduce kernels. size = grid_size + 1
+  double* partial_double2; //!< temporary array used in reduce kernels. size = (grid_size + 1)*2
+  double* partial_double3; //!< temporary array used in reduce kernels. size = (grid_size + 1)*3
 
 private:
   void UpdateBeamFromFile(std::string r_file);
   void InitPhiAvgGood();
   void SaveBeam(std::vector<double*>& r_beam, uint*& r_loss, uint*& r_lloss);
   void RestoreBeam(std::vector<double*>& r_beam, uint*& r_loss, uint*& r_lloss);
-  //! Flag to indicate whether the memory is allocated. 
-  //! Default to be false. Set to be true only by AllocateBeam() 
-  bool init_done; 
-  //! Copy of the initial beam, x, xp, y, yp, phi, w coordinates, 
-  //! used in save and restore beam to initial condition
-  std::vector<double*> beam_0;
-  //! Copy of the inital beam transverse loss coordinate, 
-  //! used in save and restore beam to initial condition
-  uint* loss_0; 
-  //! Copy of the inital beam longitudinal loss coordinate, 
-  //! used in save and restore beam to initial condition
-  uint* lloss_0; 
-  //! Copy of the design energy, used in save and restore beam to initial 
-  //! condition
-  double design_w_0; 
-  //! Copy of the frequency, used in save and restore beam to initial condition
-  double freq_0; 
-  //! Copy of the current, used in save and restore beam to initial condition
-  double current_0; 
-  //! Copy of the intermediate beam, x, xp, y, yp, phi, w coordinates, used in 
-  //! save and restore beam to intermediate condition
-  std::vector<double*> beam_1; 
-  //! Copy of the intermediate beam transverse loss coordinate, 
-  //! used in save and restore beam to intermediate condition
-  uint* loss_1; 
-  //! Copy of the intermediate beam longitudinal loss coordinate, 
-  //! used in save and restore beam to intermediate condition
-  uint* lloss_1; 
-  //! Copy of the design energy, used in save and restore beam to 
-  //! intermediate condition
-  double design_w_1; 
-  //! Copy of the frequency, used in save and restore beam to 
-  //! intermediate condition
-  double freq_1; 
-  //! Copy of the current, used in save and restore beam to initial condition.
-  double current_1; 
+  bool init_done; //!< flag to indicate whether the memory is allocated. Default to be false. Set to be true only by AllocateBeam() 
+  std::vector<double*> beam_0; //!< copy of the initial beam, x, xp, y, yp, phi, w coordinates, used in save and restore beam to initial condition.
+  uint* loss_0; //!< copy of the inital beam transverse loss coordinate, used in save and restore beam to initial condition.
+  uint* lloss_0; //!< copy of the inital beam longitudinal loss coordinate, used in save and restore beam to initial condition.
+  double design_w_0; //!< copy of the design energy, used in save and restore beam to initial condition.
+  double freq_0; //!< copy of the frequency, used in save and restore beam to initial condition.
+  double current_0; //!< copy of the current, used in save and restore beam to initial condition.
+  std::vector<double*> beam_1; //!< copy of the intermediate beam, x, xp, y, yp, phi, w coordinates, used in save and restore beam to intermediate condition.
+  uint* loss_1; //!< copy of the intermediate beam transverse loss coordinate, used in save and restore beam to intermediate condition.
+  uint* lloss_1; //!< copy of the intermediate beam longitudinal loss coordinate, used in save and restore beam to intermediate condition.
+  double design_w_1; //!< copy of the design energy, used in save and restore beam to intermediate condition.
+  double freq_1; //!< copy of the frequency, used in save and restore beam to intermediate condition.
+  double current_1; //!< copy of the current, used in save and restore beam to initial condition.
 };
 
 #endif
